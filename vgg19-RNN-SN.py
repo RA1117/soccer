e2916da3498428a0ebcfda92447186ee6f18b8d8
RA1117 <135476493+RA1117@users.noreply.github.com>
2024-01-14 11:50:55 +0900

    Add files via upload

---
+++ vgg19-RNN-SN.py
@@ -0,0 +1,249 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#pip install -q git+https://github.com/tensorflow/docs
+#pip install imutils
+#from tensorflow_docs.vis import embed
+from tensorflow import keras
+from imutils import paths
+
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+import imageio
+import cv2
+import os
+import warnings
+
+IMG_SIZE = 224
+BATCH_SIZE = 512
+EPOCHS = 30
+
+MAX_SEQ_LENGTH = 100
+NUM_FEATURES = 25088
+
+train_df = pd.read_csv("/home/arai/video2/train/videotrain.csv")
+test_df = pd.read_csv("/home/arai/video2/test/videotest.csv")
+
+print(f"Total videos for training: {len(train_df)}")
+print(f"Total videos for testing: {len(test_df)}")
+
+train_df.head(10)
+
+def crop_center_square(frame):
+    y, x = frame.shape[0:2]
+    min_dim = min(y, x)
+    start_x = (x // 2) - (min_dim // 2)
+    start_y = (y // 2) - (min_dim // 2)
+    return frame[start_y : start_y + min_dim, start_x : start_x + min_dim]
+
+
+def load_video(path, max_frames=0, resize=(IMG_SIZE, IMG_SIZE)):
+    cap = cv2.VideoCapture(path)
+    frames = []
+    try:
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                break
+            frame = crop_center_square(frame)
+            frame = cv2.resize(frame, resize)
+            frame = frame[:, :, [2, 1, 0]]
+            frames.append(frame)
+
+            if len(frames) == max_frames:
+                break
+    finally:
+        cap.release()
+    return np.array(frames)
+
+def build_feature_extractor():
+    
+    feature_extractor = keras.applications.VGG19(
+        weights="imagenet",
+        include_top=False,
+        pooling=None,
+        input_shape=(IMG_SIZE, IMG_SIZE, 3),
+    )
+    
+    preprocess_input = keras.applications.vgg19.preprocess_input
+
+    inputs = keras.Input((IMG_SIZE, IMG_SIZE, 3))
+    preprocessed = preprocess_input(inputs)
+
+    outputs = feature_extractor(preprocessed)
+    outputs = keras.layers.Dense(512, activation="tanh")(outputs)
+    outputs = keras.layers.Flatten()(outputs)
+    return keras.Model(inputs, outputs, name="feature_extractor")
+
+import warnings
+
+feature_extractor = build_feature_extractor()
+
+feature_extractor.summary()
+
+label_processor = keras.layers.StringLookup(
+    num_oov_indices=0, vocabulary=np.unique(train_df["event"])
+)
+print(label_processor.get_vocabulary())
+
+def prepare_all_videos(df, root_dir):
+    num_samples = len(df)
+    video_paths = df["video_name"].values.tolist()
+    #labels = df["event"].values
+    #labels = label_processor(labels[..., None]).numpy()
+    labels = pd.get_dummies(df["event"])
+
+    # `frame_masks` and `frame_features` are what we will feed to our sequence model.
+    # `frame_masks` will contain a bunch of booleans denoting if a timestep is
+    # masked with padding or not.
+    frame_masks = np.zeros(shape=(num_samples, MAX_SEQ_LENGTH), dtype="bool")
+    frame_features = np.zeros(
+        shape=(num_samples, MAX_SEQ_LENGTH, NUM_FEATURES), dtype="float32"
+    )
+
+    # For each video.
+    for idx, path in enumerate(video_paths):
+        # Gather all its frames and add a batch dimension.
+        frames = load_video(os.path.join(root_dir, path))
+        frames = frames[None, ...]
+
+        # Initialize placeholders to store the masks and features of the current video.
+        temp_frame_mask = np.zeros(shape=(1, MAX_SEQ_LENGTH,), dtype="bool")
+        temp_frame_features = np.zeros(
+            shape=(1, MAX_SEQ_LENGTH, NUM_FEATURES), dtype="float32"
+        )
+
+        # Extract features from the frames of the current video.
+        for i, batch in enumerate(frames):
+            video_length = batch.shape[0]
+            length = min(MAX_SEQ_LENGTH, video_length)
+            for j in range(length):
+                temp_frame_features[i, j, :] = feature_extractor.predict(
+                    batch[None, j, :]
+                )
+            temp_frame_mask[i, :length] = 1  # 1 = not masked, 0 = masked
+
+        frame_features[idx,] = temp_frame_features.squeeze()
+        frame_masks[idx,] = temp_frame_mask.squeeze()
+
+    return (frame_features, frame_masks), labels
+
+
+train_data, train_labels = prepare_all_videos(train_df, "/home/arai/video2/train")
+test_data, test_labels = prepare_all_videos(test_df, "/home/arai/video2/test")
+
+print(f"Frame features in train set: {train_data[0].shape}")
+print(f"Frame masks in train set: {train_data[1].shape}")
+
+# Utility for our sequence model.
+def get_sequence_model():
+    class_vocab = label_processor.get_vocabulary()
+
+    frame_features_input = keras.Input((MAX_SEQ_LENGTH, NUM_FEATURES))
+    mask_input = keras.Input((MAX_SEQ_LENGTH,), dtype="bool")
+
+    x = keras.layers.GRU(64, return_sequences=None)(
+        frame_features_input, mask=mask_input
+    )
+    #x = keras.layers.GRU(16)(x)
+    x = keras.layers.Dropout(0.4)(x)
+    #x = keras.layers.Dense(16, activation="relu")(x)
+    output = keras.layers.Dense(len(class_vocab), activation="softmax")(x)
+
+    rnn_model = keras.Model([frame_features_input, mask_input], output)
+
+    rnn_model.compile(
+        loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"]
+    )
+    return rnn_model
+
+
+# Utility for running experiments.
+def run_experiment():
+    filepath = "train/cp.ckpt"
+    checkpoint = keras.callbacks.ModelCheckpoint(
+        filepath, save_weights_only=True, save_best_only=True, verbose=1
+    )
+
+    seq_model = get_sequence_model()
+    history = seq_model.fit(
+        [train_data[0], train_data[1]],
+        train_labels,
+        validation_split=0.3,
+        epochs=EPOCHS,
+        callbacks=[checkpoint],
+    )
+
+    seq_model.load_weights(filepath)
+    _, accuracy = seq_model.evaluate([test_data[0], test_data[1]], test_labels)
+    print(f"Test accuracy: {round(accuracy * 100, 2)}%")
+    
+    metrics = ['loss', 'accuracy']  # 使用する評価関数を指定
+
+    plt.figure(figsize=(10, 5))  # グラフを表示するスペースを用意
+
+    for i in range(len(metrics)):
+
+        metric = metrics[i]
+
+        plt.subplot(1, 2, i+1)  # figureを1×2のスペースに分け、i+1番目のスペースを使う
+        plt.title(metric)  # グラフのタイトルを表示
+    
+        plt_train = history.history[metric]  # historyから訓練データの評価を取り出す
+        plt_test = history.history['val_' + metric]  # historyからテストデータの評価を取り出す
+    
+        plt.plot(plt_train, label='training')  # 訓練データの評価をグラフにプロット
+        plt.plot(plt_test, label='test')  # テストデータの評価をグラフにプロット
+        plt.legend()  # ラベルの表示
+    
+    plt.show()  # グラフの表示
+    
+    return history, seq_model
+
+
+_, sequence_model = run_experiment()
+
+def prepare_single_video(frames):
+    frames = frames[None, ...]
+    frame_mask = np.zeros(shape=(1, MAX_SEQ_LENGTH,), dtype="bool")
+    frame_features = np.zeros(shape=(1, MAX_SEQ_LENGTH, NUM_FEATURES), dtype="float32")
+
+    for i, batch in enumerate(frames):
+        video_length = batch.shape[0]
+        length = min(MAX_SEQ_LENGTH, video_length)
+        for j in range(length):
+            frame_features[i, j, :] = feature_extractor.predict(batch[None, j, :])
+        frame_mask[i, :length] = 1  # 1 = not masked, 0 = masked
+
+    return frame_features, frame_mask
+
+
+def sequence_prediction(path):
+    class_vocab = label_processor.get_vocabulary()
+
+    frames = load_video(os.path.join("home/arai/video2/test/", path))
+    frame_features, frame_mask = prepare_single_video(frames)
+    probabilities = sequence_model.predict([frame_features, frame_mask])[0]
+
+    for i in np.argsort(probabilities)[::-1]:
+        print(f"  {class_vocab[i]}: {probabilities[i] * 100:5.2f}%")
+    return frames
+
+
+def to_gif(images):
+    converted_images = images.astype(np.uint8)
+    imageio.mimsave("animation.gif", converted_images, fps=10)
+    return embed.embed_file("animation.gif")
+
+
+# test_video = np.random.choice(test_df["video_name"].values.tolist())
+test_video ="Clearence_1_562_2015-02-21.mp4"
+print(f"Test video path: {test_video}")
+test_frames = sequence_prediction(test_video)
+to_gif(test_frames[:MAX_SEQ_LENGTH])
+
